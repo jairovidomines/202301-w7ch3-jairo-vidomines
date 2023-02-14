13e6b2c1b5fd65c4b95e3ce80ca08f65fb535d31
Jairo Vidomines <jvidomines@gmail.com>
2023-02-14 19:21:16 +0100

    Create App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Layout from "../Layout/Layout";
+
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+};
+
+export default App;
