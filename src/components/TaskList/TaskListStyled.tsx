e91e52d7a4a19926af9d50a803a1f07d85a76a75
Jairo Vidomines <jvidomines@gmail.com>
2023-02-23 00:00:28 +0100

    Modify TaskList styles

---
+++ src/components/TaskList/TaskListStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TaskListStyled = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+`;
+
+export default TaskListStyled;
