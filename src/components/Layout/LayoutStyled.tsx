0373fa4865f5a73ee9906c9b189ec9a76929edec
Jairo Vidomines <jvidomines@gmail.com>
2023-02-23 00:01:29 +0100

    Modify Layout styles

---
+++ src/components/Layout/LayoutStyled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  .logo-todoist {
+    width: 175px;
+    height: auto;
+    padding: 10px 10px;
+  }
+
+  h1 {
+    font-size: 26px;
+    color: #1d1e1e;
+  }
+`;
+
+export default LayoutStyled;
